2c68ab312470d29352469323a8352ed09a0555b4
Poncho1424 <floresmedranodaniel@gmail.com>
2024-09-18 20:11:58 -0600

    Se agrego el michi boton para calcular y se cambio los colores de la letra a blanco

--- app/src/main/java/com/example/elpepe/MainActivity.java
+++ app/src/main/java/com/example/elpepe/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.elpepe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edadedit = findViewById(R.id.edad);
+        TextView resulttext = findViewById(R.id.resultado);
+        Button button = findViewById(R.id.boton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edad = edadedit.getText().toString();
+                int edadint = Integer.parseInt(edad);
+                int resultado = edadint * 7;
+                String resultadostring = "La edad del michi es: " + resultado + "años";
+                resulttext.setText(resultadostring);
+            }
+        });
     }
 }
